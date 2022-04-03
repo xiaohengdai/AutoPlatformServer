f7826d26186728f3d19bd245ca000ddcd31cd101
肖恒 <xiaoheng03@meituan.com>
2022-04-03 09:19:32 +0800

    add

--- ScheduleTask/meituan_guoyuan.py
+++ ScheduleTask/meituan_guoyuan.py
@@ -5,17 +5,11 @@ d = u2.connect('38573286')
 # print("d.info:",d.info)
 
 
-for i in range(0,10000):
+for i in range(0,1000):
     print(f"执行第{i+1}次")
     # d(textContains='79').click()
     # if d(textContains='300').exists():
     #     d(textContains='300').click()
-    if (i%50==0):
-        d.click(0.045,0.568)
+
     d.click(0.808,0.823)
     time.sleep(2)
-    if d(textContains='可开启').exists():
-        d(textContains='可开启').click()
-        time.sleep(2)
-        # d(textContains='开心收下').click()
-        d.click(0.585,0.636)
